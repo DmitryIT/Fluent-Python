db019a643102adef5e46843032f2434d87db07b4
dmitry <dmitry>
2019-06-23 23:29:57 +0200

    playing around decorators

--- comp_gens.py
+++ comp_gens.py
@@ -1,1 +1,11 @@
-'''comprechensions and generators'''
+def decorator(func):
+    def inner():
+        print('running inner()')
+    return inner
+
+#decorator(target())
+#@decorator
+def target():
+    print('runing target()')
+
+target()
