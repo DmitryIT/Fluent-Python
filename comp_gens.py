69afce5c8de8d2bb484ab1ebc007ef697f9041aa
dmitry <dmitry>
2019-09-14 13:09:32 +0200

    test

---
+++ comp_gens.py
@@ -0,0 +1,11 @@
+def decorator(func):
+    def inner():
+        print('running inner()')
+    return inner
+
+#decorator(target())
+@decorator
+def target()∏
+    print('runing target()')
+
+target()
