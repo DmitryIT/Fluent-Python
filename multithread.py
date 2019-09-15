613960d92525e6b383efab1406764dae67b27188
dmitry <dmitry>
2019-09-15 11:14:36 +0200

    single thread

--- multithread.py
+++ multithread.py
@@ -1,12 +1,27 @@
 import time
+import sys
+
+instances = [i for i in range(10)]
+
+def show(text):
+    print(text)
+    sys.stdout.flush()
 
 def do_something(instance):
-    print('running instance: ', instance)
-    time.sleep(5)
+    show('instance:' + str(instance))
+    time.sleep(1)
     return instance
 
+def do_many(instances):
+    for i in sorted(instances):
+        do_something(i)
+
 def main():
-    do_something(1)
+    do_many(instances)
 
 if __name__ == "__main__":
+    t0 = time.time()
     main()
+    elapsed = time.time() - t0
+    msg = '{} instances done in {:.2f}s'
+    print(msg.format( len(instances), elapsed ))
