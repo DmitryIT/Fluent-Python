cbf71b76703e772a898b977c5067944e1a5cb509
dmitry <dmitry>
2019-09-15 11:44:33 +0200

    change futures.map to futures.submit/as_completed

--- multithread.py
+++ multithread.py
@@ -2,7 +2,7 @@ import time
 import sys
 from concurrent import futures
 
-MAX_WORKERS = 10
+MAX_WORKERS = 2
 instances = [i for i in range(10)]
 
 def show(text):
@@ -10,14 +10,24 @@ def show(text):
     sys.stdout.flush()
 
 def do_something(instance):
-    show('instance:' + str(instance))
-    time.sleep(1)
+    time.sleep(5)
+    #show('instance:' + str(instance))
     return instance
 
 def do_many(instances):
     workers = min(len(instances), MAX_WORKERS)
+    to_do = []
     with futures.ThreadPoolExecutor(workers) as executor:
-        res = executor.map(do_something, sorted(instances))
+        for i in instances:
+            future = executor.submit(do_something, i)
+            to_do.append(future)
+            msg = 'Scheduled for {}: {}'
+            print(msg.format(i, future))
+
+        for future in futures.as_completed(to_do):
+            res = future.result()
+            msg = '{} result: {}'
+            print(msg.format(future, res))
 
 def main():
     do_many(instances)
