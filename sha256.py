e8837efe1f83deaf699724277691e7d0436d5b56
dmitry <dmitry>
2019-09-24 23:17:36 +0200

    sha256_multithread.py ready

--- sha256.py
+++ sha256.py
@@ -1,24 +1,37 @@
-import os
+import os, time
 from hashlib import sha256
 
 PATH = './data'
+BLOCK_SIZE = 65536 #64kB
 
 def file_list(path):
-    list_dir = os.listdir(path)
+    list_dir = os.scandir(path)
     file_list =[]
     for o in list_dir:
-        print(o)
-        if os.path.isfile(o):
-            file_list.append(o)
+        if os.DirEntry.is_file(o):
+            file_list.append(o.path)
     return file_list
 
 def calc_one_checksum(file):
-    with file.open() as f:
-        m = sha256()
-        m.update(f)
-        msg = 'file: {}, sha256: {}'
-        print(msg.format(f, m.digest() ))       
+    m = sha256()
+    dic = {}
+    with open(file, 'rb') as f:
+        while True:
+            data = f.read(BLOCK_SIZE)
+            if not data:
+                break
+            m.update(data)
+        #print('file: {} sha256: {}'.format(file, m.hexdigest()))
+        dic['file'] = file
+        dic['hash'] = m.hexdigest()
+        f.close()
+    return dic
+       
 
 if __name__ == "__main__":
+    t0 = time.time()
     for f in file_list(PATH):
-        print(f)
+        print(calc_one_checksum(f))
+    elapsed = time.time() - t0
+    msg = '{} files processed in {:.2f}s'
+    print(msg.format(len(file_list(PATH)), elapsed ))
